5394a3e5c33bd0d092649bd76dba411e37c4e377
Astrid Piot <astrid.piot@student.ucll.be>
2021-03-29 12:07:16 +0200

    start story 2 punt

---
+++ src/main/java/domain/Punt.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Punt {
+    public Punt(int xCoordinaat, int yCoordinaat) {
+    }
+
+    public int getX() {
+        return 0;
+    }
+
+    public int getY() {
+        return 0;
+    }
+}
