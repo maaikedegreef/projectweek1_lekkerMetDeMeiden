880f9e1eeebd77a677b09db61c74368b7a3500fe
Daan-Philips <r0791129@ucll.be>
2021-03-29 14:43:26 +0100

    lijnstuk_begin

---
+++ src/main/java/domain/LijnStuk.java
@@ -0,0 +1,45 @@
+package domain;
+
+import java.util.Objects;
+
+public class LijnStuk {
+
+    private Punt startPunt,eindPunt;
+
+    public LijnStuk(Punt startPunt, Punt eindPunt) {
+        setStartEnEindPunt(startPunt, eindPunt);
+    }
+
+    private void setStartEnEindPunt(Punt startPunt, Punt eindPunt) {
+        if (startPunt == null || eindPunt == null) throw new DomainException("StartPunt en eindPunt mogen niet null zijn");
+        if (startPunt.equals(eindPunt)) throw new DomainException("StartPunt en eindPunt mogen niet gelijk zijn");
+        this.startPunt = startPunt;
+        this.eindPunt = eindPunt;
+    }
+
+    public Punt getStartPunt() {
+        return startPunt;
+    }
+
+    public Punt getEindPunt() {
+        return eindPunt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LijnStuk lijnStuk = (LijnStuk) o;
+        return (startPunt.equals(lijnStuk.startPunt) && eindPunt.equals(lijnStuk.eindPunt)) ||
+                (startPunt.equals(lijnStuk.eindPunt) && eindPunt.equals(lijnStuk.startPunt));
+
+    }
+
+    @Override
+    public String toString() {
+        return "Lijn: " +
+                "startPunt: " + startPunt +
+                " - eindPunt: " + eindPunt;
+    }
+}
+
