7debdefa45d47770f8aec0a1ca4e09044a4ae34d
randmon <ccmarques98@gmail.com>
2021-04-01 11:09:20 +0200

    Revert "Story 12 af wejow"
    This reverts commit 7834a2142dde9ad3e5720b9ad62cd708a237d1ba.
    
    # Conflicts:
    #	src/main/java/ui/WoordRadenApp.java

--- src/main/java/ui/WoordRadenApp.java
+++ src/main/java/ui/WoordRadenApp.java
@@ -1,22 +1,37 @@
 package ui;
 
+import domain.DomainException;
 import domain.HintLetter;
 import domain.HintWoord;
 import domain.Speler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 public class WoordRadenApp {
-    public static void main(String[] args) {
+    private Label vraagLabel, hintLabel, radenLabel, resultaatLabel;
+    private TextField raden;
+    private Alert foutenboodschap = new Alert(Alert.AlertType.WARNING);
 
-        HintWoord test = new HintWoord("test");
-        System.out.println(test);
+    HintWoord woord = new HintWoord("hangman");
 
-    }
-
-
-<<<<<<< HEAD
     public WoordRadenApp(GridPane root, Speler speler) {
-=======
+        vraagLabel =  new Label("Rarara, welk woord zoeken we?");
+        hintLabel =  new Label(woord.toString());
+        radenLabel =  new Label("Geef een letter:");
+        resultaatLabel =  new Label("Super, doe zo voort!");
+        raden= new TextField();
+
+        root.add(vraagLabel,0,0);
+        root.add(hintLabel,0,1);
+        root.add(radenLabel,0,3);
+        root.add(raden,0,5);
+        root.add(resultaatLabel,0,7);
+
+        Actionevent.raadWoordEvent(root,raden,woord,speler,resultaatLabel,hintLabel,foutenboodschap);
+
         /*raden.setOnAction(eventIngaveLetter ->{
             try{
                 char letter = raden.getText().toCharArray()[0];
@@ -44,6 +59,5 @@ public class WoordRadenApp {
             }
         });*/
 
->>>>>>> parent of 7834a21... Story 12 af wejow
     }
 }
