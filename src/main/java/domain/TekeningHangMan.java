42017d0435cdda9caba66b43158479fd96dbf79c
Emile Hermans <emile.hermans@student.ucll.be>
2021-04-01 10:34:34 +0200

    A

---
+++ src/main/java/domain/TekeningHangMan.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class TekeningHangMan extends Tekening{
+    public TekeningHangMan() {
+        super();
+    }
+
+    public int getAantalOnzichtbaar() {
+        return 0;
+    }
+
+    public void voegToe(Vorm vorm) {
+
+    }
+
+    public void verwijder(Vorm vorm) {
+
+    }
+
+    public void zetVolgendeZichtbaar() {
+    }
+}
