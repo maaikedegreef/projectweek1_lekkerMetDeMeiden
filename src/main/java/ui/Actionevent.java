1fb6a5590d05b4b2ebe82b82839bd59b1f516f1c
randmon <ccmarques98@gmail.com>
2021-04-01 11:01:51 +0200

    test

--- src/main/java/ui/Actionevent.java
+++ src/main/java/ui/Actionevent.java
@@ -27,4 +27,45 @@ public class Actionevent {
         } );
 
     }
+<<<<<<< HEAD
+=======
+
+    public static void raadWoordEvent(GridPane root, TextField raden, HintWoord woord, Speler speler, Label resultaatLabel, Label hintLabel, Alert foutenboodschap) {
+        raden.setOnAction(eventIngaveLetter -> {
+            try {
+                char letter = raden.getText().toCharArray()[0];
+                //checken of letter leeg is of meer dan 1 is!!
+
+                root.getChildren().remove(resultaatLabel);
+                if (woord.raad(letter)) {
+                    resultaatLabel.setText("Super, doe zo voort!");
+                    root.add(resultaatLabel, 0, 7);
+                } else {
+                    resultaatLabel.setText("Helaas volgende keer beter");
+                    root.add(resultaatLabel, 0, 7);
+                }
+                root.getChildren().remove(hintLabel);
+                hintLabel.setText(woord.toString());
+                root.add(hintLabel, 0, 1);
+                raden.clear();
+
+            } catch (DomainException | ArrayIndexOutOfBoundsException e) {
+                raden.clear();
+                foutenboodschap.setTitle("Warning");
+                foutenboodschap.setContentText(e.getMessage());
+                foutenboodschap.showAndWait();
+            }
+            if (!woord.isGeraden()) {
+                raadWoordEvent(root, raden, woord, speler, resultaatLabel, hintLabel, foutenboodschap);
+            } else{
+                root.getChildren().clear();
+                Text uitvoer = new Text();
+                uitvoer.setText("Goed gedaan " + speler.getNaam() + "! Je hebt het woord geraden");
+                root.add(uitvoer, 0, 0);
+
+            }
+
+        });
+    }
+>>>>>>> parent of 7834a21... Story 12 af wejow
 }
--- src/main/java/ui/WoordRadenApp.java
+++ src/main/java/ui/WoordRadenApp.java
@@ -14,6 +14,36 @@ public class WoordRadenApp {
     }
 
 
+<<<<<<< HEAD
     public WoordRadenApp(GridPane root, Speler speler) {
+=======
+        /*raden.setOnAction(eventIngaveLetter ->{
+            try{
+                char letter = raden.getText().toCharArray()[0];
+
+                if (woord.isGeraden()){
+                    root.getChildren().clear();
+                    Text uitvoer = new Text();
+                    uitvoer.setText("Goed gedaan "+speler.getNaam()+"! Je hebt het woord geraden");
+                    root.add(uitvoer, 0, 0);;
+                } else {
+                    if (woord.raad(letter)){
+                        resultaatLabel.setText("Super, doe zo voort!");
+                        root.add(resultaatLabel,0,7);
+                    } else {
+                        resultaatLabel.setText("Helaas volgende keer beter");
+                        root.add(resultaatLabel,0,7);
+                    }
+                    hintLabel.setText(woord.toString());
+                }
+            } catch (DomainException e){
+                raden.clear();
+                foutenboodschap.setTitle("Warning");
+                foutenboodschap.setContentText(e.getMessage());
+                foutenboodschap.showAndWait();
+            }
+        });*/
+
+>>>>>>> parent of 7834a21... Story 12 af wejow
     }
 }
